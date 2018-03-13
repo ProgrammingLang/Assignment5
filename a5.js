af2cd38acf9d49da0abaa072f70dee2c3642ee5a
Nou Yang <mnyang1991@gmail.com>
2018-03-12 22:29:18 -0500

    Add files via upload

---
+++ a5.js
@@ -0,0 +1,83 @@
+var fp = require('./fp');
+var util = require('util');
+
+if ( ! exports )
+   var exports = [ ];
+
+// Your solution for problem 1 must appear between this and matching
+// end comment below
+
+var searchList = function(ns, target) {
+  var cps_searchList = function(ns, target, k) {
+    if(fp.isNull(ns)){
+      return [];
+    }
+    else if(fp.isEq(fp.hd(ns), target)){
+      return k([target]);
+    }
+    else {
+      return cps_searchList(fp.tl(ns), target, function(x){
+        return k(fp.cons(fp.hd(ns), x));
+      })
+    }
+  };
+
+  return cps_searchList(ns, target, function(x) { return x; });
+};
+
+
+////////// End of code for problem 1 ////////////////////
+
+
+// Your solution for problem 2 must appear between this and matching
+// end comment below
+
+var b2d = function (b) {
+  return 0;
+};
+
+
+
+// ////////// End of code for problem 2 ////////////////////
+//
+// Your solution for problem 3 must appear between this and matching
+// end comment below
+
+var b2d_cps = function (b) {
+    return 0;
+};
+
+
+// ////////// End of code for problem 3 ////////////////////
+//
+
+////////// All test cases you add must be below this comment line
+
+////////// Everything below this line will be stripped away when your
+////////// submission is evaluated
+
+console.log("Testing Problem 1");
+console.log(searchList([1,2,3,4,5], 4, []));
+console.log(searchList([1,2,3,4,5], 6, []));
+
+
+console.log("Testing Problem 2");
+console.log( b2d([0]) );
+console.log( b2d([1]) );
+console.log( b2d([0,1]) );
+console.log( b2d([1,1]) );
+console.log( b2d([0,0,1,1]) );
+console.log( b2d([1,0,1,0,0,1,1]) );
+console.log( b2d([1,0,1,0,0,0,0]) );
+
+console.log("Testing Problem 3");
+console.log( b2d_cps([0]) );
+console.log( b2d_cps([0,0]) );
+console.log( b2d_cps([0,0,0,0]) );
+console.log( b2d_cps([1]) );
+console.log( b2d_cps([1,0]) );
+console.log( b2d_cps([0,1]) );
+console.log( b2d_cps([1,1]) );
+console.log( b2d_cps([0,0,1,1]) );
+console.log( b2d_cps([1,0,1,0,0,1,1]) );
+console.log( b2d_cps([1,0,1,0,0,0,0]) );
